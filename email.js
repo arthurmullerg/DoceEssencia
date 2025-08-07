6f9743852f77c6d6753761774b70ebe4a057668f
Arthur <arthurmullerg@gmail.com>
2025-08-07 16:34:50 -0300

    form com email

---
+++ email.js
@@ -0,0 +1,31 @@
+ emailjs.init({
+        publicKey: "3Dk7zR3nHyxHLbNgW",
+      });
+
+document.getElementById('form-cont').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value
+    };
+     
+    const seviceID = "service_zjm3eos";
+    const templateID = "template_h9cqdy6";
+
+    emailjs.send(serviceID, templateID, formData)
+    .then(() => {
+        Toastify({
+            text: "Formulário enviado com sucesso",
+            duration: 3000
+        }).showToast();
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar o e-mail:", error);
+    });
+});
+
+ 
+
+    
